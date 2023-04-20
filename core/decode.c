aa477024d930d21230ecdf70cd3583b00ad5e463
Florian B <briand.flo13@gmail.com>
2023-04-20 15:25:27 +0200

    amélioration code

--- core/decode.c
+++ core/decode.c
@@ -37,30 +37,55 @@ void sauvegarder_registres(char *nom_fichier) {
     fclose(fichier);
 }
 
+int getBCC(long instruction) {
+    return (instruction & 0xf0000000) >> 24;
+}
 
+int getOpcode(long instruction) {
+    return (instruction & 0x00f00000) >> 20;
+}
+
+int getOpe1(long instruction) {
+    return (instruction & 0x000f0000) >> 16;
+}
+
+int getOpe2(long instruction) {
+    return (instruction & 0x0000f000) >> 12;
+}
+
+int getDest(long instruction) {
+    return (instruction & 0x00000f00) >> 8;
+}
+
+int getIV(long instruction) {
+    return (instruction & 0x000000ff);
+}
+
+int getIVflag(long instruction) {
+    return (instruction & 0x00800000) >> 23;
+}
 
 int decode(long instruction) {
 
     int res;
 
     //Recuperer le opcode
-    int opcode = (instruction & 0x00f00000) >> 20;
+    int opcode = getOpcode(instruction);
 
     //Recuperer le ope1
-    int ope1 = (instruction & 0x000f0000) >> 16;
+    int ope1 = getOpe1(instruction);
 
     //Recuperer le ope2
-    int ope2 = (instruction & 0x0000f000) >> 12;
+    int ope2 = getOpe2(instruction);
 
     //Recuperer le destanation
-    int dest = (instruction & 0x00000f00) >> 8;
+    int dest = getDest(instruction);
 
     //Recuperer le IV
-    int IV = (instruction & 0x000000ff);
+    int IV = getIV(instruction);
 
     //Recuperer le IVflag
-    int IVflag = (instruction & 0x00800000) >> 23;
-
+    int IVflag = getIVflag(instruction);
 
 
     printf("instruction = %x\n", instruction);
@@ -71,7 +96,7 @@ int decode(long instruction) {
     printf("IV = %x\n", IV);
     printf("IVflag = %x\n", IVflag);
 
-    res=execute(opcode,ope1,ope2,dest,IV,IVflag);
+    res = execute(opcode, ope1, ope2, dest, IV, IVflag);
 
     return res;
 }     //return statement added for clarity and to adhere to best practices of always having a return statement in a function.
--- core/decode.h
+++ core/decode.h
@@ -10,6 +10,14 @@ int decode(long instruction);
 void lire_fichier_registres(char *nom_fichier);
 void sauvegarder_registres(char *nom_fichier);
 
+int getBCC(long instruction);
+int getOpcode(long instruction);
+int getOpe1(long instruction);
+int getOpe2(long instruction);
+int getDest(long instruction);
+int getIV(long instruction);
+int getIVflag(long instruction);
+
 
 
 #endif //VIRTUALCOREIMPLEMENTATION_DECODE_H
